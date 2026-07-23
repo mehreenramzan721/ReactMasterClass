5a61969b09e1fd53c6f3b5b6fc38a570fe8f5225
mehreenramzan721 <mehreenramzan721@gmail.com>
2026-07-23 21:19:31 +0500

    commits

--- ReactPro2/src/components/ConditionalComponent.jsx
+++ ReactPro2/src/components/ConditionalComponent.jsx
@@ -3,11 +3,20 @@
 export default function ConditionalComponent(){
     let messageOne = <h1>This is a message 1</h1>
     let messageTwo = <h1>This is a message 2</h1>
-    const display = true;
+    //component variable method:
+
+    let message;
+
+    const display = false;
 
     if(display){
-        return messageOne;
-    }else{
-        return messageTwo;
-    }
+            return message = <h1>This is a message 1</h1>;
+        }else{
+            return message = <h1>This is a message 2</h1>;
+        }
+    // if(display){
+    //     return messageOne;
+    // }else{
+    //     return messageTwo;
+    // }
 }
