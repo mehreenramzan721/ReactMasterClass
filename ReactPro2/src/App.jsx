eacf7e97fc3cea81d8116d112c40fdbe7601405d
mehreenramzan721 <mehreenramzan721@gmail.com>
2026-07-23 21:45:31 +0500

    commits

--- ReactPro2/src/App.jsx
+++ ReactPro2/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import ConditionalComponent from './components/ConditionalComponent'
+import Message from './components/Message'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <ConditionalComponent/>
+    {/* <ConditionalComponent/> */}
+    <Message/>
     </>
   )
 }
---
+++ ReactPro2/src/components/Message.jsx
@@ -0,0 +1,5 @@
+export default function Message(){
+    <>
+        <button>click here!</button>
+    </>
+}
