4072f316d2d4882927a8aefb4a3023fb286ba936
mehreenramzan721 <mehreenramzan721@gmail.com>
2026-07-24 12:25:22 +0500

    spread operator

--- ReactPro2/src/components/Form.jsx
+++ ReactPro2/src/components/Form.jsx
@@ -1,16 +1,24 @@
 import {useState} from 'react'
 
 export default function Form(){
-    const [name, setName ] = useState("");
+    // const [name, setName ] = useState("");
+    // handling multiple inputs 
+    const [name, setName ] = useState({firstName:"", lastName:""});
+
     function handleChange(e){
         console.log(e);
         console.log(e.target.value)
-        setName(e.target.value)
+        setName({...name,firstName:e.target.value})
     }
     return(
     <>
     <form>
-        <input onChange={(e)=> handleChange(e)}  type="text"  value = {name} />
+        <input onChange={(e)=> handleChange(e)}  
+        type="text"  
+        value = {name.firstName} />
+        <input onChange ={(e)=> setName({...name, lastName:e.target.value})}
+        type="text"
+        value={name.lastName}/>
     </form>
     </>
     )
