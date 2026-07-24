f734c1187cadd5dc09882ed76a56c05365191b06
mehreenramzan721 <mehreenramzan721@gmail.com>
2026-07-24 23:28:21 +0500

    completed

--- ReactPro3_TodoApp/src/components/Todo.jsx
+++ ReactPro3_TodoApp/src/components/Todo.jsx
@@ -3,15 +3,19 @@ import {useState} from 'react'
 import Header from './Header'
 import Form from './Form'
 import TodoList from './TodoList'
+import Footer from './Footer'
 export default function Todo(){
    
     const [todos, setTodos] = useState([]);
+    const completedTodos = todos.filter((todo)=>todo.done).length;
+    const totalTodos = todos.length;
 
     return(
         <>
         <Header/>
         <Form todos={todos} setTodos={setTodos}/>
         <TodoList todos={todos} setTodos={setTodos}/>
+        <Footer completedTodos={completedTodos} totalTodos={totalTodos}/>
         </>
     )
 }
--- ReactPro3_TodoApp/src/components/TodoList.jsx
+++ ReactPro3_TodoApp/src/components/TodoList.jsx
@@ -1,9 +1,10 @@
 import TodoItem from './TodoItem'
 import styles from './todolist.module.css'
 export default function Todolist({todos, setTodos}){
+    const sortedTodos = todos.slice().sort((a,b)=>(Number(a.done)-Number(b.done)));
     return(
         <div className={styles.list}>
-        {todos.map((item)=><TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos}/>)}
+        {sortedTodos.map((item)=><TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos}/>)}
         </div>
     )
 }
